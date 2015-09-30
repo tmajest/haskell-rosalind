b6154aa644705306aaf1e9b6bb4a2297387d1350
Tony Majestro <tonymajestro@gmail.com>
2015-09-30 09:33:26 -0700

    first commit

---
+++ dna.hs
@@ -0,0 +1,14 @@
+
+countBases :: String -> [Integer]
+countBases [] = [0, 0, 0, 0]
+countBases (x:xs) 
+    | x == 'A' = [rest !! 0 + 1, rest !! 1, rest !! 2, rest !! 3]
+    | x == 'C' = [rest !! 0, rest !! 1 + 1, rest !! 2, rest !! 3]
+    | x == 'G' = [rest !! 0, rest !! 1, rest !! 2 + 1, rest !! 3]
+    | x == 'T' = [rest !! 0, rest !! 1, rest !! 2, rest !! 3 + 1]
+    where rest = (countBases xs)
+
+main = do
+    dna <- getLine 
+    let baseCount = countBases dna
+    putStrLn $ unwords (map show baseCount)
